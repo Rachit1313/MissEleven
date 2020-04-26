4f24be0551a66308e7b1fa5e344187554a10f567
satanicsoul <58979792+satanicsoul@users.noreply.github.com>
2020-04-26 20:24:20 +0530

    Update and rename .chatbot.py to chatbot.py

--- eleven/modules/chatbot.py
+++ eleven/modules/chatbot.py
@@ -45,7 +45,7 @@ def remove_chat(update, context):
         msg.reply_text("AI disabled successfully!")
         
         
-def check_message(update, message):
+def check_message(context):
     reply_msg = message.reply_to_message
     if message.text.lower() == "eleven":
         return True
